f5805a20366962470fa523256a46129186d53be9
Jorick DEFRAINE <j.defraine@yeswehack.com>
2026-05-06 10:25:13 +0200

    Update for production

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  turbopack: {
-    root: "/home/jdefraine/Documents/PERSO/umyo-app",
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
